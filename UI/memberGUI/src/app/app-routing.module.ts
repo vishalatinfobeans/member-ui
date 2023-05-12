8486d30de38bfa080592b615f37b21d16c093360
vishalatinfobeans <vishal.purohit@infobeans.com>
2023-05-12 15:11:30 +0530

    added authentication shared module and dashboard component

--- UI/memberGUI/src/app/app-routing.module.ts
+++ UI/memberGUI/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { PageNotFoundComponent } from './authentication/page-not-found/page-not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
