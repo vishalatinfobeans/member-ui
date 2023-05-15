6d1e3cb4848924818513b87bcbbf19597053bd32
spatidarit <siddharth.patidar@infobeans.com>
2023-05-15 14:32:30 +0530

    navber badges  visaible on login

--- UI/memberGUI/src/app/shared/header/header.component.ts
+++ UI/memberGUI/src/app/shared/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { AuthenticateService } from 'src/app/authentication/authenticate.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  @Input()  isLogin:boolean =false;
+export class HeaderComponent{
+  constructor(public authService:AuthenticateService){
+  }
 }
