74198530abc297805a4ae76cbb2db2e9c077134a
vishalatinfobeans <vishal.purohit@infobeans.com>
2023-05-18 17:24:04 +0530

    pwa added

--- UI/memberGUI/src/app/app.module.ts
+++ UI/memberGUI/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './shared/header/header.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,14 @@ import { AuthenticationModule } from './authentication/authentication.module';
     AppRoutingModule,
     NgbModule,
     SharedModule,
-    AuthenticationModule
+    AuthenticationModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      // enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      // registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
